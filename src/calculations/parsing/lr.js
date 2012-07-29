f393705074c10481e8953d101067b8b57433208d
Michael Daines <michael@mdaines.com>
2012-07-29 14:50:47 -0500

    fix lr0 classification calculation

--- src/calculations/parsing/lr.js
+++ src/calculations/parsing/lr.js
@@ -437,14 +437,19 @@
     
     var i, s;
     var table = grammar.calculate("parsing.lr.lr0_table");
+    var terminals = grammar.calculate("grammar.terminals");
     
     for (i = 0; i < table.length; i++) {
         
       if (table[i].reduce.length > 1)
         return { member: false, reason: "it contains a reduce-reduce conflict" };
-      
-      if (Set.any(table[i].shift) && table[i].reduce.length > 0)
-        return { member: false, reason: "it contains a shift-reduce conflict" };
+        
+      if (table[i].reduce.length > 0) {
+        for (s in table[i].shift) {
+          if (terminals[s])
+            return { member: false, reason: "it contains a shift-reduce conflict" };
+        }
+      }
       
     }
     
