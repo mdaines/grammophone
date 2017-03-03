3ec81bf77d9def5c8a0bd36547bfc400bc443566
Michael Daines <mdaines@fastmail.com>
2017-03-03 17:21:00 -0500

    Remove unused productionsEqual function.

--- grammar/test/parsing-test.js
+++ grammar/test/parsing-test.js
@@ -3,26 +3,6 @@
 const assert = require('assert');
 const Grammar = require('../index');
 
-function productionsEqual(a, b) {
-  if (a.length !== b.length) {
-    return false;
-  }
-
-  for (let i = 0; i < a.length; i++) {
-    if (a[i].length !== b[i].length) {
-      return false;
-    }
-
-    for (let j = 0; j < a[i].length; j++) {
-      if (a[i][j] !== b[i][j]) {
-        return false;
-      }
-    }
-  }
-
-  return true;
-}
-
 function assertParseProductions(expected, spec) {
   let parse = Grammar.parse(spec);
   if (parse.error) {
