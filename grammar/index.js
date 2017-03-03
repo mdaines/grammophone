27ed7073aeda642cd08c99c017170de1a19a1237
Michael Daines <mdaines@fastmail.com>
2017-03-03 17:44:40 -0500

    Fix lint errors in Grammar and Relation. Turn off forin and remove checks from Sets.

--- grammar/index.js
+++ grammar/index.js
@@ -1,237 +1,228 @@
+'use strict';
+
 const Calculations = require('./calculations');
 const Parser = require('./parser');
-const END = require('./symbols').END
+const END = require('./symbols').END;
 
-
-function parse(spec) {
+class Grammar {
   
-  var i, j;
+  static parse(spec) {
   
-  if (spec.match(/^\s*$/))
-    return { spec: spec };
+    if (spec.match(/^\s*$/)) {
+      return { spec: spec };
+    }
   
-  try {
+    try {
     
-    // Parser gives us rules in the following form:
-    //
-    //   { nt: "A", p: [["a", "b"], []] }
-    //
-    // We want an array of productions in this form:
-    //
-    //   [["A", "a", "b"], ["A"]]
-    //
-    // Note that depending on the grammar specification, productions
-    // for a particular nonterminal may be at different places in the
-    // list. We want to preserve the order in the user's input.
+      // Parser gives us rules in the following form:
+      //
+      //   { nt: "A", p: [["a", "b"], []] }
+      //
+      // We want an array of productions in this form:
+      //
+      //   [["A", "a", "b"], ["A"]]
+      //
+      // Note that depending on the grammar specification, productions
+      // for a particular nonterminal may be at different places in the
+      // list. We want to preserve the order in the user's input.
   
-    var rules = Parser.parse(spec);
-    var productions = [];
+      let rules = Parser.parse(spec);
+      let productions = [];
   
-    for (i = 0; i < rules.length; i++) {
-      for (j = 0; j < rules[i].p.length; j++) {
-        productions.push([rules[i].nt].concat(rules[i].p[j]));
+      for (let i = 0; i < rules.length; i++) {
+        for (let j = 0; j < rules[i].p.length; j++) {
+          productions.push([rules[i].nt].concat(rules[i].p[j]));
+        }
+      }
+  
+      return { grammar: new Grammar(productions), spec: spec };
+    
+    } catch (e) {
+    
+      return { error: e, spec: spec };
+    
+    }
+  
+  }
+
+  constructor(productions) {
+  
+    // Check for reserved and empty symbols
+
+    for (let i = 0; i < productions.length; i++) {
+      for (let j = 0; j < productions[i].length; j++) {
+    
+        if (productions[i][j].match(/^Grammar\./)) {
+          throw "Reserved symbol " + productions[i][j] + " may not be part of a production";
+        }
+      
+        if (productions[i][j] === "") {
+          throw "An empty symbol may not be part of a production";
+        }
+    
+      }
+    }
+
+    // Assign productions
+
+    this.productions = productions;
+  
+    // Initialize calculations memoization
+  
+    this.calculations = {};
+  
+  }
+
+  calculate(name) {
+  
+    if (typeof Calculations[name] === "undefined") {
+      throw "Undefined grammar calculation " + name;
+    }
+
+    if (typeof this.calculations[name] === "undefined") {
+      this.calculations[name] = Calculations[name](this);
+    }
+
+    return this.calculations[name];
+  
+  }
+
+  transform(transformation) {
+  
+    let productions = this.productions.slice();
+  
+    transformation.changes.forEach(function(change) {
+    
+      if (change.operation === "delete") {
+        productions.splice(change.index, 1);
+      } else if (change.operation === "insert") {
+        productions.splice(change.index, 0, change.production);
+      }
+    
+    });
+  
+    return new Grammar(productions);
+  
+  }
+
+  getFirst(symbols) {
+  
+    let first = this.calculate("grammar.first");
+    let nullable = this.calculate("grammar.nullable");
+    let terminals = this.calculate("grammar.terminals");
+    let nonterminals = this.calculate("grammar.nonterminals");
+
+    let result = {};
+
+    for (let i = 0; i < symbols.length; i++) {
+  
+      let s = symbols[i];
+  
+      if (s === END) {
+    
+        result[s] = true;
+        break;
+    
+      } else if (terminals[s]) {
+    
+        result[s] = true;
+        break;
+    
+      } else if (nonterminals[s]) {
+    
+        for (let k in first[s]) {
+          result[k] = true;
+        }
+    
+        if (!nullable[s]) {
+          break;
+        }
+    
+      } else {
+    
+        throw "Unexpected symbol " + s;
+    
+      }
+  
+    }
+
+    return result;
+
+  }
+
+  isNullable(symbols) {
+  
+    let nullable = this.calculate("grammar.nullable");
+    let terminals = this.calculate("grammar.terminals");
+    let nonterminals = this.calculate("grammar.nonterminals");
+
+    for (let i = 0; i < symbols.length; i++) {
+  
+      let s = symbols[i];
+  
+      if (nonterminals[s]) {
+    
+        if (!nullable[s]) {
+          return false;
+        }
+    
+      } else if (terminals[s]) {
+    
+        return false;
+    
+      } else {
+    
+        throw "Unexpected symbol " + s;
+    
+      }
+  
+    }
+
+    return true;
+
+  }
+
+  copyProductions() {
+  
+    let result = [];
+  
+    for (let i = 0; i < this.productions.length; i++) {
+      result[i] = [];
+    
+      for (let j = 0; j < this.productions[i].length; j++) {
+        result[i][j] = this.productions[i][j];
       }
     }
   
-    return { grammar: new klass(productions), spec: spec };
-    
-  } catch (e) {
-    
-    return { error: e, spec: spec };
-    
-  }
+    return result;
   
-}
-
-// instance
-
-function initialize(productions) {
-  
-  // Check for reserved and empty symbols
-
-  var i, j;
-
-  for (i = 0; i < productions.length; i++) {
-    for (j = 0; j < productions[i].length; j++) {
-    
-      if (productions[i][j].match(/^Grammar\./))
-        throw "Reserved symbol " + productions[i][j] + " may not be part of a production";
-      
-      if (productions[i][j] === "")
-        throw "An empty symbol may not be part of a production";
-    
-    }
   }
 
-  // Assign productions
-
-  this.productions = productions;
+  toString() {
   
-  // Initialize calculations memoization
+    let result = "";
   
-  this.calculations = {};
-  
-}
-
-function calculate(name) {
-  
-  if (typeof Calculations[name] === "undefined")
-    throw "Undefined grammar calculation " + name;
-
-  if (typeof this.calculations[name] === "undefined")
-    this.calculations[name] = Calculations[name](this);
-
-  return this.calculations[name];
-  
-}
-
-function transform(transformation) {
-  
-  var productions = this.productions.slice();
-  
-  transformation.changes.forEach(function(change) {
+    for (let i = 0; i < this.productions.length; i++) {
     
-    if (change.operation === "delete")
-      productions.splice(change.index, 1);
-    else if (change.operation === "insert")
-      productions.splice(change.index, 0, change.production);
+      result += this.productions[i][0];
+      result += " ->";
     
-  });
-  
-  return new Grammar(productions);
-  
-}
-
-function getFirst(symbols) {
-
-  var i, k;
-  var s, t;
-  var result;
-  
-  var first = this.calculate("grammar.first");
-  var nullable = this.calculate("grammar.nullable");
-  var terminals = this.calculate("grammar.terminals");
-  var nonterminals = this.calculate("grammar.nonterminals");
-
-  result = {};
-
-  for (i = 0; i < symbols.length; i++) {
-  
-    s = symbols[i];
-  
-    if (s === END) {
+      for (let j = 1; j < this.productions[i].length; j++) {
+        result += " " + this.productions[i][j];
+      }
     
-      result[s] = true;
-      break;
-    
-    } else if (terminals[s]) {
-    
-      result[s] = true;
-      break;
-    
-    } else if (nonterminals[s]) {
-    
-      for (k in first[s])
-        result[k] = true;
-    
-      if (!nullable[s])
-        break;
-    
-    } else {
-    
-      throw "Unexpected symbol " + s;
+      result += " .\n";
     
     }
   
-  }
-
-  return result;
-
-}
-
-function isNullable(symbols) {
-
-  var i, s;
-  
-  var nullable = this.calculate("grammar.nullable");
-  var terminals = this.calculate("grammar.terminals");
-  var nonterminals = this.calculate("grammar.nonterminals");
-
-  for (i = 0; i < symbols.length; i++) {
-  
-    s = symbols[i];
-  
-    if (nonterminals[s]) {
-    
-      if (!nullable[s])
-        return false;
-    
-    } else if (terminals[s]) {
-    
-      return false;
-    
-    } else {
-    
-      throw "Unexpected symbol " + s;
-    
-    }
+    return result;
   
   }
 
-  return true;
-
-}
-
-function copyProductions() {
-  
-  var i, j;
-  var result = [];
-  
-  for (i = 0; i < this.productions.length; i++) {
-    result[i] = [];
-    
-    for (j = 0; j < this.productions[i].length; j++) {
-      result[i][j] = this.productions[i][j];
-    }
-  }
-  
-  return result;
-  
-}
-
-function toString() {
-  
-  var i, j;
-  var result = "";
-  
-  for (i = 0; i < this.productions.length; i++) {
-    
-    result += this.productions[i][0];
-    result += " ->";
-    
-    for (j = 1; j < this.productions[i].length; j++)
-      result += " " + this.productions[i][j];
-    
-    result += " .\n";
-    
-  }
-  
-  return result;
-  
 }
 
 // export
 
-var klass = initialize;
+Grammar.END = END;
 
-klass.parse = parse;
-klass.END = END;
-
-klass.prototype.calculate = calculate;
-klass.prototype.transform = transform;
-klass.prototype.getFirst = getFirst;
-klass.prototype.isNullable = isNullable;
-klass.prototype.copyProductions = copyProductions;
-klass.prototype.toString = toString;
-
-module.exports = klass;
+module.exports = Grammar;
--- grammar/sets.js
+++ grammar/sets.js
@@ -1,42 +1,36 @@
-var Sets = {
-  
+'use strict';
+
+const Sets = {
   count: function(set) {
+    let result = 0;
     
-    var n;
-    var result = 0;
-    
-    for (n in set)
+    for (let n in set) {
       result++;
+    }
       
     return result;
     
   },
   
   any: function(set) {
-    
-    var n;
-    
-    for (n in set)
+    for (let n in set) {
       return true;
+    }
     
     return false;
-    
   },
   
   intersection: function(a, b) {
+    let result = {};
     
-    var result = {};
-    var k;
-    
-    for (k in a) {
-      if (b[k])
+    for (let k in a) {
+      if (b[k]) {
         result[k] = true;
+      }
     }
     
     return result;
-    
   }
-  
 };
 
 module.exports = Sets;
