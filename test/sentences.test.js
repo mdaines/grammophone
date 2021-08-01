c8ca93caa4794437e62888637adb51abc7adba09
Michael Daines <mdaines@fastmail.com>
2021-08-01 15:32:28 -0400

    Add more tests for queue depth limit

--- test/sentences.test.js
+++ test/sentences.test.js
@@ -1,17 +1,28 @@
 const Grammar = require("../src/grammar");
+const exampleFixtures = require("./fixtures/example_grammars.js");
 
-describe("grammar.sentences", function() {
-  it("queue depth limit works", function() {
-    let spec = `
-    a1 -> x .
-    a1 -> a2 x a2 .
-    a2 -> a3 x a3 .
-    a3 -> a4 x a4 .
-    a4 -> a5 x a5 .
-    a5 -> a1 x a1 .
-    `;
+const queueDepthFixtures = [
+  `a1 -> x .
+a1 -> a2 x a2 .
+a2 -> a3 x a3 .
+a3 -> a4 x a4 .
+a4 -> a5 x a5 .
+a5 -> a1 x a1 .`,
+  `A -> A B | B .
+B -> A | x .`
+];
 
+function check(spec) {
+  it(spec, function() {
     let grammar = Grammar.parse(spec).grammar;
     grammar.calculate("grammar.sentences");
   });
+}
+
+describe("grammar.sentences", function() {
+  queueDepthFixtures.forEach(check);
+
+  for (let k in exampleFixtures) {
+    check(exampleFixtures[k]);
+  }
 });
