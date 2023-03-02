7b1089af3e3ad91820145c2067e0348c041da4e6
Michael Daines <mdaines@fastmail.com>
2023-03-02 13:02:01 -0500

    Convert application controller to a component

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const { render, Component } = require("preact");
+const { useReducer, useEffect } = require("preact/hooks");
+const { reducer, init } = require("./reducer.js");
+const ApplicationComponent = require("./components/application_component.js");
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, "# Type a grammar here:\n\n", init);
+
+  function onHashChange() {
+    let path = window.location.hash.slice(1);
+    if (path == "") {
+      path = "/";
+    }
+
+    dispatch({ type: "setPath", path });
+  }
+
+  useEffect(() => {
+    window.addEventListener("hashchange", onHashChange);
+    return () => window.removeEventListener("hashchange", onHashChange);
+  }, []);
+
+  return (
+    <ApplicationComponent
+      spec={state.spec}
+      error={state.error}
+      grammar={state.grammar}
+      path={state.path}
+      mode={state.mode}
+      transformStack={state.transformStack}
+      transformIndex={state.transformIndex}
+      updateSpec={(newValue) => { dispatch({ type: "setSpec", spec: newValue }); }}
+      edit={() => dispatch({ type: "edit" })}
+      transform={() => dispatch({ type: "transform" })}
+      analyze={() => dispatch({ type: "analyze" })}
+      undoTransformation={() => { dispatch({ type: "undoTransformation" }); }}
+      redoTransformation={() => { dispatch({ type: "redoTransformation" }); }}
+      applyTransformation={(t) => { dispatch({ type: "applyTransformation", transformation: t }); }}
+    />
+  );
+}
+
+render(<App />, document.getElementById("app"));
--- src/index.js
+++
@@ -1,5 +0,0 @@
-var ApplicationController = require("./controllers/application_controller");
-
-window.onload = function() {
-  new ApplicationController(document.body);
-}
