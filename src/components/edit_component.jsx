a63eed45426e663a716b3195af7afa0cf9c7397c
Michael Daines <1383+mdaines@users.noreply.github.com>
2023-11-29 05:26:11 -0500

    Use a placeholder instead of comment

--- src/components/edit_component.jsx
+++ src/components/edit_component.jsx
@@ -2,7 +2,7 @@ export default function({ spec, specChanged }) {
   return (
     <div id="edit">
       <div className="spec-wrap">
-        <textarea className="spec" onInput={(e) => { specChanged(e.target.value); }} defaultValue={spec} />
+        <textarea className="spec" onChange={(e) => { specChanged(e.target.value); }} value={spec} placeholder="Type a grammar here..." />
       </div>
     </div>
   );
