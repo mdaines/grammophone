480f80c05cbfcec1c4430c7cc09bf77d4f9c0195
Michael Daines <1383+mdaines@users.noreply.github.com>
2024-02-11 12:56:47 -0500

    Interpret escapes in symbols

--- src/parser/index.js
+++ src/parser/index.js
@@ -1,5 +1,25 @@
 import { parser } from "./rules.js";
 
+const ESCAPES = {
+  "0": "\0",
+  "n": "\n",
+  "r": "\r",
+  "v": "\v",
+  "t": "\t",
+  "b": "\b",
+  "f": "\f",
+  "\n": "",
+  "\r": ""
+};
+
+function interpretEscapes(str) {
+  return str.replaceAll(/\\(?:x([0-9a-fA-F]{2})|u(?:\{([0-9a-fA-F]+)\}|([0-9a-fA-F]{4})))/g, function(_, hex, u1, u2) {
+    return String.fromCodePoint(parseInt(hex || u1 || u2, 16));
+  }).replaceAll(/\\(.)/gs, function(_, char) {
+    return ESCAPES[char] || char;
+  });
+}
+
 export default function(src) {
   let tree = parser.parse(src);
   let cursor = tree.cursor();
@@ -27,7 +47,7 @@ export default function(src) {
         production.push(symbol);
       }
     } else if (cursor.name === "QuotedSymbol") {
-      let symbol = src.slice(cursor.from + 1, cursor.to - 1);
+      let symbol = interpretEscapes(src.slice(cursor.from + 1, cursor.to - 1));
 
       if (typeof production === "undefined") {
         head = symbol;
